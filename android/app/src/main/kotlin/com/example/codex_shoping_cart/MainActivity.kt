267947d641c94dc63614bc1ee18c6bd9a674ae6c
fasil2401 <fasil2401@gmail.com>
2022-07-03 19:31:33 +0530

    initial commit

---
+++ android/app/src/main/kotlin/com/example/codex_shoping_cart/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.codex_shoping_cart
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:codex_shoping_cart/views/shopping_page.dart';
+import 'package:flutter/material.dart';
+
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: ShoppingPage(),
+    );
+  }
+}
